0c0844e262630f845f57aade50168800a97f8b11
Steve <steveoscar12@gmail.com>
2024-02-05 17:48:37 +0100

    our Cpde

--- src/main/scala/de/ddm/Sindy.scala
+++ src/main/scala/de/ddm/Sindy.scala
@@ -2,39 +2,27 @@ package de.ddm
 
 import org.apache.spark.sql.{Dataset, Row, SparkSession}
 
-import scala.collection.mutable.ListBuffer
+object SINDyAlgorithm {
 
-object Sindy {
-
-  private def readData(input: String, spark: SparkSession): Dataset[Row] = {
+  private def loadTimeSeriesData(inputPath: String, spark: SparkSession): Dataset[Row] = {
     spark
       .read
-      .option("inferSchema", "false")
-      .option("header", "true")
-      .option("quote", "\"")
-      .option("delimiter", ";")
-      .csv(input)
+      .option("inferSchema", "true")
+      .csv(inputPath)
   }
 
-
-  def discoverINDs(inputs: List[String], spark: SparkSession): Unit = {
+  def applySINDyAlgorithm(inputPaths: List[String], spark: SparkSession): Unit = {
     import spark.implicits._
 
-    val result =
-      inputs.map(input => readData(input, spark))
-        .map(table => {
-          val columns = table.columns
-          table.flatMap(row => for (i <- columns.indices) yield (columns(i), row.getString(i)))})
-        .reduce((set1, set2) => set1 union set2)
-        .groupByKey(t => t._2)
-        .mapGroups((_, iterator) => iterator.map(_._1).toSet)
-        .flatMap(Set => Set
-          .map(currentAttribute => (currentAttribute, Set.filter(attribute => !attribute.equals(currentAttribute)))))
-        .groupByKey(row => row._1)
-        .mapGroups((key, iterator) => (key, iterator.map(row => row._2).reduce((set1, set2) => set1.intersect(set2))))
-        .collect() // Here spark stops
+    // Load and merge time series data
+    val combinedData = inputPaths.map(input => loadTimeSeriesData(input, spark))
+      .reduce((dataset1, dataset2) => dataset1.union(dataset2))
 
-    result.sortBy(tuple => tuple._1)
-      .foreach(IND => if (IND._2.nonEmpty) println(IND._1 + " -> " + IND._2.mkString(",")))
+    // Implement the SINDy algorithm for sparse identification of nonlinear dynamics
+    // TODO: Add your SINDy algorithm logic here
+    // ...
+
+    // Example: Display the combined time series data
+    combinedData.show()
   }
 }
